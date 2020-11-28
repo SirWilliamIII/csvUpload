750d179cc65a40476b995eed83ca119bd68174d7
Will Carpenter <willcarpenterwastaken@gmail.com>
2020-11-28 12:44:46 -0600

    format csv output

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const logger = require('morgan')
 
 const indexRouter = require('./routes/index')
 
-
 const app = express()
 
 const bodyParser = require('body-parser')
@@ -18,31 +17,36 @@ app.set('view engine', 'hbs')
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({
-    extended: false
+	extended: false
 }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 
+
+
+
 app.use(bodyParser.urlencoded({
-        extended: true
-    }))
-    // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404))
+	extended: true
+}))
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	next(createError(404))
 })
 
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message
+	res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-    // render the error page
-    res.status(err.status || 500)
-    res.render('error')
+	// render the error page
+	res.status(err.status || 500)
+	res.render('error')
 })
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const { EOL } = require('os');
+const { convertArrayToCSV } = require('convert-array-to-csv');
+const converter = require('convert-array-to-csv');
+
+
+const multer = require('multer')
+const csv = require('@fast-csv/parse')
+
+const upload = multer({dest: 'tmp/csv/'})
 
 const db = require("../db/db")
 
-
-
-
 const addEntry = db.addEntry
+
 let getAll = db.getAllfromTable
 
 
@@ -29,22 +36,30 @@ router.get('/data', function(req, res, next) {
 
 
 
-router.post('/submit', (req, res) => {
+router.post('/submit', upload.single('csvFile'),(req, res) => {
 
     const name = req.body.name
     const company = req.body.company
-    const csv = req.body.csvText
 
-	// const csvFile = req.body.csvFile
-	//
-	// console.log(csvData)
 
-	addEntry("csvFiles", name, company, csv)
+	let fileRows = []
 
-	res.redirect('/')
+
+	csv.parseFile(req.file.path, {objectMode: true})
+	    .on('error', error => console.error(error))
+	    .on('data', (row) => {
+	    	// console.log(`ROW=${row}`)
+		    fileRows.push(row)
+	    })
+	    .on('end', (rowCount) => {
+	    	console.log(`Parsed ${rowCount} rows`)
+		    const csvFromArrayOfObjects = convertArrayToCSV(fileRows,{ separator: ',' });
+		    addEntry("csvFiles", name, company, csvFromArrayOfObjects)
+			res.redirect('/')
+	    })
+
+
 })
-
-
 
 
 module.exports = router
