e46072f724f24a9b6984967419934734e71f2dc5
Will Carpenter <willcarpenterwastaken@gmail.com>
2020-11-28 12:57:38 -0600

    removed unused db methods

--- db/db.js
+++ db/db.js
@@ -6,53 +6,53 @@ AWS.config.update({
 })
 
 const DynamoDB = new AWS.DynamoDB()
+//
+// function createTable(tableName) {
+//     const params = {
+//         TableName: tableName,
+//         KeySchema: [{
+//             AttributeName: "title",
+//             KeyType: "HASH"
+//         }],
+//         AttributeDefinitions: [{
+//             AttributeName: "title",
+//             AttributeType: "S"
+//         }],
+//         ProvisionedThroughput: {
+//             ReadCapacityUnits: 10,
+//             WriteCapacityUnits: 10
+//         }
+//     }
+//
+//     DynamoDB.createTable(params, (e, data) => {
+//         if (e) {
+//             console.error("Unable to create table", e)
+//         } else {
+//             console.log('Created Table', data)
+//         }
+//     })
+// }
 
-function createTable(tableName) {
-    const params = {
-        TableName: tableName,
-        KeySchema: [{
-            AttributeName: "title",
-            KeyType: "HASH"
-        }],
-        AttributeDefinitions: [{
-            AttributeName: "title",
-            AttributeType: "S"
-        }],
-        ProvisionedThroughput: {
-            ReadCapacityUnits: 10,
-            WriteCapacityUnits: 10
-        }
-    }
-
-    DynamoDB.createTable(params, (e, data) => {
-        if (e) {
-            console.error("Unable to create table", e)
-        } else {
-            console.log('Created Table', data)
-        }
-    })
-}
-
-function getEntryByKey(tableName, key) {
-    let items = []
-    const params = {
-        TableName: tableName,
-        Key: {
-            title: {
-                S: key
-            }
-        }
-    }
-    DynamoDB.getItem(params, (e, data) => {
-        if (e) {
-            console.error("Unable to find movie")
-        } else {
-            items = data.Item
-
-        }
-    })
-    return items
-}
+// function getEntryByKey(tableName, key) {
+//     let items = []
+//     const params = {
+//         TableName: tableName,
+//         Key: {
+//             title: {
+//                 S: key
+//             }
+//         }
+//     }
+//     DynamoDB.getItem(params, (e, data) => {
+//         if (e) {
+//             console.error("Unable to find movie")
+//         } else {
+//             items = data.Item
+//
+//         }
+//     })
+//     return items
+// }
 
 
 function addEntry(table, name, company, csv) {
@@ -84,6 +84,5 @@ function addEntry(table, name, company, csv) {
 }
 
 module.exports = {
-    addEntry,
-    getEntryByKey
+    addEntry
 }
